0cd074e8ee549972ef6fd4dfc4d88e280e752a97
dienkhai <dienkhai>
2023-10-10 11:25:43 +0700

    ette

---
+++ Untitled-1.cs
@@ -0,0 +1,265 @@
+>
+
+								        
+													<div class="item">
+					<div class="products">
+						<div class="hot-deal-wrapper">
+							<div class="image">
+								<img src="admin/productimages/<?php echo htmlentities($rws['id']);?>/<?php echo htmlentities($rws['productImage1']);?>"  width="200" height="334" alt="">
+							</div>
+							
+						</div><!-- /.hot-deal-wrapper -->
+
+						<div class="product-info text-left m-t-20">
+							<h3 class="name"><a href="product-details.php?pid=<?php echo htmlentities($rws['id']);?>"><?php echo htmlentities($rws['productName']);?></a></h3>
+							<div class="rating rateit-small"></div>
+
+							<div class="product-price">	
+								<span class="price">
+									Rs. <?php echo htmlentities($rws['productPrice']);?>.00
+								</span>
+									
+							    <span class="price-before-discount">Rs.<?php echo htmlentities($row['productPriceBeforeDiscount']);?></span>					
+							
+							</div><!-- /.product-price -->
+							
+						</div><!-- /.product-info -->
+
+						<div class="cart clearfix animate-effect">
+							<div class="action">
+								
+								<div class="add-cart-button btn-group">
+									<button class="btn btn-primary icon" data-toggle="dropdown" type="button">
+								<?php if($row['productAvailability']=='In Stock'){?>
+										<button class="btn btn-primary icon" data-toggle="dropdown" type="button">
+								<i class="fa fa-shopping-cart"></i>													
+							</button>
+							<a href="category.php?page=product&action=add&id=<?php echo $row['id']; ?>">
+							<button class="btn btn-primary" type="button">Add to cart</button></a>
+								<?php } else {?>
+							<div class="action" style="color:red">Out of Stock</div>
+					<?php } ?>
+															
+								</div>
+								
+							</div><!-- /.action -->
+						</div><!-- /.cart -->
+					</div>	
+					</div>		
+					<?php } ?>        
+						
+	    
+    </div><!-- /.sidebar-widget -->
+</div>
+
+<!-- ============================================== COLOR: END ============================================== -->
+				</div>
+			</div><!-- /.sidebar -->
+<?php 
+$ret=mysqli_query($con,"select * from products where id='$pid'");
+while($row=mysqli_fetch_array($ret))
+{
+
+?>
+
+
+			<div class='col-md-9'>
+				<div class="row  wow fadeInUp">
+					     <div class="col-xs-12 col-sm-6 col-md-5 gallery-holder">
+    <div class="product-item-holder size-big single-product-gallery small-gallery">
+
+        <div id="owl-single-product">
+
+ <div class="single-product-gallery-item" id="slide1">
+                <a data-lightbox="image-1" data-title="<?php echo htmlentities($row['productName']);?>" href="admin/productimages/<?php echo htmlentities($row['id']);?>/<?php echo htmlentities($row['productImage1']);?>">
+                    <img class="img-responsive" alt="" src="assets/images/blank.gif" data-echo="admin/productimages/<?php echo htmlentities($row['id']);?>/<?php echo htmlentities($row['productImage1']);?>" width="370" height="350" />
+                </a>
+            </div>
+
+
+
+
+            <div class="single-product-gallery-item" id="slide1">
+                <a data-lightbox="image-1" data-title="<?php echo htmlentities($row['productName']);?>" href="admin/productimages/<?php echo htmlentities($row['id']);?>/<?php echo htmlentities($row['productImage1']);?>">
+                    <img class="img-responsive" alt="" src="assets/images/blank.gif" data-echo="admin/productimages/<?php echo htmlentities($row['id']);?>/<?php echo htmlentities($row['productImage1']);?>" width="370" height="350" />
+                </a>
+            </div><!-- /.single-product-gallery-item -->
+
+            <div class="single-product-gallery-item" id="slide2">
+                <a data-lightbox="image-1" data-title="Gallery" href="admin/productimages/<?php echo htmlentities($row['id']);?>/<?php echo htmlentities($row['productImage2']);?>">
+                    <img class="img-responsive" alt="" src="assets/images/blank.gif" data-echo="admin/productimages/<?php echo htmlentities($row['id']);?>/<?php echo htmlentities($row['productImage2']);?>" />
+                </a>
+            </div><!-- /.single-product-gallery-item -->
+
+            <div class="single-product-gallery-item" id="slide3">
+                <a data-lightbox="image-1" data-title="Gallery" href="admin/productimages/<?php echo htmlentities($row['id']);?>/<?php echo htmlentities($row['productImage3']);?>">
+                    <img class="img-responsive" alt="" src="assets/images/blank.gif" data-echo="admin/productimages/<?php echo htmlentities($row['id']);?>/<?php echo htmlentities($row['productImage3']);?>" />
+                </a>
+            </div>
+
+        </div><!-- /.single-product-slider -->
+
+
+        <div class="single-product-gallery-thumbs gallery-thumbs">
+
+            <div id="owl-single-product-thumbnails">
+                <div class="item">
+                    <a class="horizontal-thumb active" data-target="#owl-single-product" data-slide="1" href="#slide1">
+                        <img class="img-responsive"  alt="" src="assets/images/blank.gif" data-echo="admin/productimages/<?php echo htmlentities($row['id']);?>/<?php echo htmlentities($row['productImage1']);?>" />
+                    </a>
+                </div>
+
+            <div class="item">
+                    <a class="horizontal-thumb" data-target="#owl-single-product" data-slide="2" href="#slide2">
+                        <img class="img-responsive" width="85" alt="" src="assets/images/blank.gif" data-echo="admin/productimages/<?php echo htmlentities($row['id']);?>/<?php echo htmlentities($row['productImage2']);?>"/>
+                    </a>
+                </div>
+                <div class="item">
+
+                    <a class="horizontal-thumb" data-target="#owl-single-product" data-slide="3" href="#slide3">
+                        <img class="img-responsive" width="85" alt="" src="assets/images/blank.gif" data-echo="admin/productimages/<?php echo htmlentities($row['id']);?>/<?php echo htmlentities($row['productImage3']);?>" height="200" />
+                    </a>
+                </div>
+
+               
+               
+                
+            </div><!-- /#owl-single-product-thumbnails -->
+
+            
+
+        </div>
+
+    </div>
+</div>     			
+
+
+
+
+					<div class='col-sm-6 col-md-7 product-info-block'>
+						<div class="product-info">
+							<h1 class="name"><?php echo htmlentities($row['productName']);?></h1>
+<?php $rt=mysqli_query($con,"select * from productreviews where productId='$pid'");
+$num=mysqli_num_rows($rt);
+{
+?>		
+							<div class="rating-reviews m-t-20">
+								<div class="row">
+									<div class="col-sm-3">
+										<div class="rating rateit-small"></div>
+									</div>
+									<div class="col-sm-8">
+										<div class="reviews">
+											<a href="#" class="lnk">(<?php echo htmlentities($num);?> Reviews)</a>
+										</div>
+									</div>
+								</div><!-- /.row -->		
+							</div><!-- /.rating-reviews -->
+<?php } ?>
+							<div class="stock-container info-container m-t-10">
+								<div class="row">
+									<div class="col-sm-3">
+										<div class="stock-box">
+											<span class="label">Availability :</span>
+										</div>	
+									</div>
+									<div class="col-sm-9">
+										<div class="stock-box">
+											<span class="value"><?php echo htmlentities($row['productAvailability']);?></span>
+										</div>	
+									</div>
+								</div><!-- /.row -->	
+							</div>
+
+
+
+<div class="stock-container info-container m-t-10">
+								<div class="row">
+									<div class="col-sm-3">
+										<div class="stock-box">
+											<span class="label">Product Brand :</span>
+										</div>	
+									</div>
+									<div class="col-sm-9">
+										<div class="stock-box">
+											<span class="value"><?php echo htmlentities($row['productCompany']);?></span>
+										</div>	
+									</div>
+								</div><!-- /.row -->	
+							</div>
+
+
+<div class="stock-container info-container m-t-10">
+								<div class="row">
+									<div class="col-sm-3">
+										<div class="stock-box">
+											<span class="label">Shipping Charge :</span>
+										</div>	
+									</div>
+									<div class="col-sm-9">
+										<div class="stock-box">
+											<span class="value"><?php if($row['shippingCharge']==0)
+											{
+												echo "Free";
+											}
+											else
+											{
+												echo htmlentities($row['shippingCharge']);
+											}
+
+											?></span>
+										</div>	
+									</div>
+								</div><!-- /.row -->	
+							</div>
+
+							<div class="price-container info-container m-t-20">
+								<div class="row">
+									
+
+									<div class="col-sm-6">
+										<div class="price-box">
+											<span class="price">Rs. <?php echo htmlentities($row['productPrice']);?></span>
+											<span class="price-strike">Rs.<?php echo htmlentities($row['productPriceBeforeDiscount']);?></span>
+										</div>
+									</div>
+
+
+
+
+									<div class="col-sm-6">
+										<div class="favorite-button m-t-10">
+											<a class="btn btn-primary" data-toggle="tooltip" data-placement="right" title="Wishlist" href="product-details.php?pid=<?php echo htmlentities($row['id'])?>&&action=wishlist">
+											    <i class="fa fa-heart"></i>
+											</a>
+											
+											</a>
+										</div>
+									</div>
+
+								</div><!-- /.row -->
+							</div><!-- /.price-container -->
+
+	
+
+
+
+
+							<div class="quantity-container info-container">
+								<div class="row">
+									
+									<div class="col-sm-2">
+										<span class="label">Qty :</span>
+									</div>
+									
+									<div class="col-sm-2">
+										<div class="cart-quantity">
+											<div class="quant-input">
+								                <div class="arrows">
+								                  <div class="arrow plus gradient"><span class="ir"><i class="icon fa fa-sort-asc"></i></span></div>
+								                  <div class="arrow minus gradient"><span class="ir"><i class="icon fa fa-sort-desc"></i></span></div>
+								                </div>
+								                <input type="text" value="1">
+							              </div>
+							            </div>
+									</div>
